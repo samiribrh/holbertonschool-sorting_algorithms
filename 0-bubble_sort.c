15664813a9a60f39b93bb646c1845ea691c49e87
samiribrh <samiribrahimov2277@gmail.com>
2024-04-18 23:31:16 +0400

    Condition changed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,7 +17,7 @@ void bubble_sort(int *array, size_t size)
 		swap = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
-			if (array[j] >= array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
